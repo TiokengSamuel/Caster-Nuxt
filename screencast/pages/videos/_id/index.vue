063e52d3c3a58abdda86f9ac6fffde53c11204a1
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-18 19:09:04 +0100

    id

--- screencast/pages/videos/_id/index.vue
+++ screencast/pages/videos/_id/index.vue
@@ -6,28 +6,7 @@
 
 <script>
 export default {
-  data() {
-        return{
-            videos: [{
-                    id: "16",
-                    name: "Intro in Nuxt"
-                },
-                {
-                    id:"1",
-                    name: "Intro in Vue"
-                },
-                {
-                    id: "10",
-                    name: "Advanced concepts"
-
-                }]
-        }
-    },
-    computed: {
-      videos() {
-        return this.videos.find(v => v.id == this.$route.params.id)
-      }
-    }
+  
 }
 </script>
 
