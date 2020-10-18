adf10b9b3795e52735130e82e7f72f985a397a74
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-18 18:41:21 +0100

    params

--- screencast/pages/videos/_id.vue
+++ screencast/pages/videos/_id.vue
@@ -6,7 +6,23 @@
 
 <script>
 export default {
+  data() {
+        return{
+            videos: [{
+                    id: "16",
+                    name: "Intro in Nuxt"
+                },
+                {
+                    id:"1",
+                    name: "Intro in Vue"
+                },
+                {
+                    id: "10",
+                    name: "Advanced concepts"
 
+                }]
+        }
+    },
 }
 </script>
 
