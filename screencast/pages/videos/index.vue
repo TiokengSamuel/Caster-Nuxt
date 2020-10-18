9a779b2c95ad117aaba2845c345dec26e5d4f6b9
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-18 18:29:33 +0100

    routes

---
+++ screencast/pages/videos/index.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="videos">
+  <nuxt-/>
+  <h1>videos</h1>
+  <div v-for="video in videos" :key="video.id">
+    <p>{{video.name}}</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            videos: [{
+                    id: "16",
+                    name: "Intro in Nuxt"
+                },
+                {
+                    id:"1",
+                    name: "Intro in Vue"
+                },
+                {
+                    id: "10",
+                    name: "Advanced concepts"
+
+                }]
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
